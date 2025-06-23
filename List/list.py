69c17db9df6fc590557593ea7b270e797459e401
rahulkumar2022 <rahul.kumarcs22@gmail.com>
2025-06-24 00:50:54 +0530

    list and operators

---
+++ List/list.py
@@ -0,0 +1,60 @@
+myList = ["1","8","2", "3", "4", "5"]
+print(myList)
+myList.append("6")
+print(myList)
+myList.insert(0, "0")
+print(myList)
+myList.remove("3")
+print(myList)
+print(len(myList))
+myList.pop()
+print(myList)
+myList.sort()
+print(myList)
+myList.reverse()
+print(myList)
+
+print(myList[0])
+print(myList[1:3])
+print(myList[-2])
+
+myList2 = myList.copy()
+myList2.reverse()
+print(myList2)
+myList2[1] = "99"
+print(myList2)
+print(myList2.count("99"))
+myList2[1:3]= ["88", "77"]
+print(myList2)
+myList2.insert(2, "66")
+print(myList2)
+myList2.extend(["11", "22"])
+print(myList2)
+myList2.append("33")
+print(myList2)
+myList2.extend(myList)
+print(myList2)
+myList2.pop();
+myList2.pop(0)
+print(myList2)
+del myList2[0]
+print(myList2)
+# myList2.clear()
+# print(myList2)
+
+# loop through list
+for item in myList2:
+    print(item)
+
+# loop through list with index
+for i in range(len(myList2)):
+    print(f"Index {i}: {myList2[i]}")
+
+# using while loop
+i = 0
+while i < len(myList2):
+    print(f"While Index {i}: {myList2[i]}")
+    i += 1
+
+# List comprehension
+[print(x) for x in myList2]
---
+++ operators/operators.py
@@ -0,0 +1,43 @@
+x = 2
+y = 3
+
+x = x + y
+print(x)  # Output: 5
+
+x = 2
+x += y
+print(x)  # Output: 5
+
+x %= y # same as x = x % y
+print(x)  # Output: 2
+
+x **= y # same as x = x ** y
+print(x)  # Output: 8
+
+x = 2
+# & -> and
+# | -> or
+# ^ -> xor
+x &= y  # same as x = x & y
+print(x)  # Output: 2
+
+x = 2
+x |= y  # same as x = x | y
+print(x)  # Output: 3   
+x = 2
+x ^= y  # same as x = x ^ y
+print(x)  # Output: 1       
+x = 2
+x <<= y  # same as x = x << y
+print(x)  # Output: 16
+x = 2
+x >>= y  # same as x = x >> y
+print(x)  # Output: 0       
+x = 2
+x //= y  # same as x = x // y
+print(x)  # Output: 0   
+x = 2
+x = ~x  
+print(x)  # Output: -3   
+x = 2
+
